65557c0d57f95a5c6baada649370ce3c4aa7250d
Peter Shen <shenp@wellsfargo.com>
2015-11-21 11:01:48 -0800

    complete assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+#
+# Matrix Inversion
+# This R program contains functions that perform "continuous" matrix inversion vector.
+# One can repeatly use this fuction to perform complex matrix inversion.
+#
 
 ## Write a short comment describing this function
-
+# makeCacheMatrix caches the input square matrix to global environment
+#
+library(matrixcalc)
 makeCacheMatrix <- function(x = matrix()) {
-
+# check in x is a square matrix
+        if (!is.square.matrix(x))
+                stop("Input is not a sqaure matrix")
+        
+        mx <- NULL
+        set <- function(y) {
+                x <<- y
+                mx <<- NULL
+        }
+        get <- function() x
+        
+        setinverse <- function(minv) mx <<- minv
+        getinverse <- function() mx
+        
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+# cacheSolve returns inverse of a makeCacheMatrix object.
+# If an inverse already cached, it returns the cached inverse matrix.
+# Otherwise, it calculates the inverse matrix, caches it and returns it.
+#
 
 cacheSolve <- function(x, ...) {
+        mx <- x$getinverse()
+        if(!is.null(mx)) {
+                message("getting cached inverse matrix...")
+                return(mx)
+        }
         ## Return a matrix that is the inverse of 'x'
+        org <- x$get()
+        mx <- solve(org)
+        x$setinverse(mx)
+        mx
 }
